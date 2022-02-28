5b49a64e3940b4003bbd8bfd52cf0828016cafd3
Milan <jmilan.97@gmail.com>
2022-03-01 00:16:59 +0100

    cors

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 
 var apiRouter = require('./routes/api');
@@ -22,6 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 app.use('/api', apiRouter);
 app.use('/admin', adminRouter);
